711a3a947151bd3f60ae091a42997177b97a1003
Hang Phung <hang.phungthu01@gmail.com>
2024-06-14 01:17:37 +0700

    Add download file - synthetic-4 dataset

---
+++ Dataset/download.sh
@@ -0,0 +1,8 @@
+gdown --id 1bFEYX3CmNf821fKc-KGZqtgCXw5rFFV3
+unzip synthetic_4_dataset.zip
+unzip cinic10-py.zip
+unzip unzip MMAFEDB.zip
+mv synthetic_4_dataset/cinic10-py .
+mv synthetic_4_dataset/MMAFEDB .
+mv synthetic_4_dataset/MNISTM .
+mv synthetic_4_dataset/FashionMNIST .
