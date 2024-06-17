734b7d2dc3f7a9fdf0fa28217c43654ecf69634f
Hang Phung <hang.phungthu01@gmail.com>
2024-06-17 18:57:59 +1000

    Add download synthetic 4-dataset

--- Dataset/download.sh
+++ Dataset/download.sh
@@ -1,8 +1,9 @@
+pip install gdown==4.5.4 --no-cache-dir 
 gdown --id 1bFEYX3CmNf821fKc-KGZqtgCXw5rFFV3
 unzip synthetic_4_dataset.zip
-unzip cinic10-py.zip
-unzip unzip MMAFEDB.zip
-mv synthetic_4_dataset/cinic10-py .
-mv synthetic_4_dataset/MMAFEDB .
-mv synthetic_4_dataset/MNISTM .
-mv synthetic_4_dataset/FashionMNIST .
+unzip synthetic_4_dataset/cinic10-py.zip
+unzip synthetic_4_dataset/MMAFEDB.zip
+# mv synthetic_4_dataset/cinic10-py .
+# mv synthetic_4_dataset/MMAFEDB .
+# mv synthetic_4_dataset/MNISTM .
+# mv synthetic_4_dataset/FashionMNIST .
